e167e5ea47d044f8bb8af4b09828417a23b3fb57
lananhtran11 <133591090+lananhtran11@users.noreply.github.com>
2023-07-17 20:46:47 +0700

    buoi8
    buoi8

---
+++ Category/edit-car.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'pdo.php';
+require_once 'helper.php';
+
+$request = $_POST;
+
+$category = [
+    'name' => $request['name'],
+    'id' => $request['id'],
+];
+
+$getinf = new Query();
+$getinf->update($category);
+// update($category);
+redirectHome();
